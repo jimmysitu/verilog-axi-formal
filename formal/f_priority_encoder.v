f004ecbab89dffb4e18c56266ff71d1016ff83a9
Jimmy Situ <web@jimmystone.cn>
2021-04-29 00:07:14 +0800

    priority_encoder prf/cvr done

--- formal/f_priority_encoder.v
+++ formal/f_priority_encoder.v
@@ -43,19 +43,52 @@ module f_priority_encoder #
 parameter LEVELS = WIDTH > 2 ? $clog2(WIDTH) : 1;
 parameter W = 2**LEVELS;
 
-    priority_encoder dut(/*AUTOINST*/
-                         // Outputs
-                         .output_valid          (output_valid),
-                         .output_encoded        (output_encoded[$clog2(WIDTH)-1:0]),
-                         .output_unencoded      (output_unencoded[WIDTH-1:0]),
-                         // Inputs
-                         .input_unencoded       (input_unencoded[WIDTH-1:0]));
+    priority_encoder #(/*AUTOINSTPARAM*/
+                       // Parameters
+                       .WIDTH           (WIDTH),
+                       .LSB_PRIORITY    (LSB_PRIORITY),
+                       .LEVELS          (LEVELS),
+                       .W               (W))
+        dut(/*AUTOINST*/
+            // Outputs
+            .output_valid               (output_valid),
+            .output_encoded             (output_encoded[$clog2(WIDTH)-1:0]),
+            .output_unencoded           (output_unencoded[WIDTH-1:0]),
+            // Inputs
+            .input_unencoded            (input_unencoded[WIDTH-1:0]));
+    
+    // Assume properties
 
-
-// Cover properties
-    cover property(
-        (output_valid == 1'b1)
+    // Proof properties
+    // output need to sync between encoded and unencoded
+    prf_sync: assert property(
+        (1<<output_encoded) == output_unencoded
     );
+
+    always @(*) begin
+        if(input_unencoded)
+            prf_input: assert property(
+                (1<<output_encoded) & input_unencoded
+            );
+    end
+
+
+    // Cover properties
+    // output need to sync between encoded and unencoded
+    cvr_onehot: cover property(
+        (1<<output_encoded) == output_unencoded
+    );
+
+    always @(*) begin
+        if(input_unencoded)
+            cvr_vld: cover property(
+                (output_valid == 1'b1)
+            );
+        else
+            cvr_invld: cover property(
+                (output_valid == 1'b0)
+            );
+    end
 
 endmodule
 
